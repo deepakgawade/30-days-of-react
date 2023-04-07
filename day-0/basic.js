e2fb3b3a5b0a01ebdd7e0bcbff66b6e2e95953db
deepakgawade <deepakgawade22@gmail.com>
2023-04-07 16:32:53 +0530

    arrays

--- day-0/basic.js
+++ day-0/basic.js
@@ -1,1 +1,25 @@
 console.log("Started with git ");
+
+const PI = 3.14;
+let js = "Javascript";
+const character = js.split("");
+console.log(character);
+
+const arr = Array(10).fill(null);
+console.log(arr);
+
+const firtist = [1, 2, 3];
+const secondlist = [4, 5, 6];
+
+console.log([...firtist, ...secondlist]);
+
+console.log(firtist.concat(secondlist));
+
+const fruits = ["bannana", "pineapple", "grapes", "watermelon"];
+let item = fruits.indexOf("bannana");
+if (item != -1) {
+  console.log("Found");
+} else {
+  console.log("you should buy one");
+}
+console.log(fruits.includes("Bannana"));
